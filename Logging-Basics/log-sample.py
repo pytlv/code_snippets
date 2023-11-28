fe3c95be8d3fc197617246e3c928fdc2b9038405
BH <herzman@gmail.com>
2023-11-28 10:54:42 +0200

    updating corey's code to write log in the current library

--- Logging-Basics/log-sample.py
+++ Logging-Basics/log-sample.py
@@ -1,5 +1,6 @@
 
 import logging
+import os
 
 # DEBUG: Detailed information, typically of interest only when diagnosing problems.
 
@@ -11,7 +12,12 @@ import logging
 
 # CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
 
-logging.basicConfig(filename='test.log', level=logging.DEBUG,
+script_folder = os.path.dirname(os.path.abspath(__file__))
+print("Script folder:", script_folder)
+
+file_name = os.path.join(script_folder, "test.log")
+
+logging.basicConfig(filename=file_name, level=logging.DEBUG,
                     format='%(asctime)s:%(levelname)s:%(message)s')
 
 
@@ -39,13 +45,15 @@ num_1 = 20
 num_2 = 10
 
 add_result = add(num_1, num_2)
-logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+
+logging.debug(f'Add: {num_1} + {num_2} = {add_result}')
+print(f'Add: {num_1} + {num_2} = {add_result}')
 
 sub_result = subtract(num_1, num_2)
-logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+logging.debug(f'Sub: {num_1} - {num_2} = {sub_result}')
 
 mul_result = multiply(num_1, num_2)
-logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+logging.debug(f'Mul: {num_1} * {num_2} = {mul_result}')
 
 div_result = divide(num_1, num_2)
-logging.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+logging.debug(f'Div: {num_1} / {num_2} = {div_result}')
